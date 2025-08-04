cc720f3546bd687057dde98b190dda4737324229
Mai <mai@tilde.team>
2025-08-03 23:28:39 -0300

    refactor: use lib crate

--- src/api.rs
+++ src/api.rs
@@ -1,19 +1,31 @@
 use std::sync::Arc;
 
-use axum::{extract::{Path, State}, routing::get, Json, Router};
+use axum::{extract::{Path, State}, http::Method, routing::get, Json, Router};
+use rand::Rng;
+use tower_http::cors::{Any, CorsLayer};
 
-use crate::enemy::{get_all_enemies, get_random_enemy, Enemy, EnemyDiff};
+use eowordle_lib::{list_enemies, Enemy, EnemyDiff};
+
+pub fn get_random_enemy() -> Enemy {
+    let enemies = list_enemies();
+    let mut rng = rand::rng();
+    let enemy = enemies.get(rng.random_range(0..enemies.len())).unwrap().clone();
+
+    println!("The daily enemy is:\n{enemy:?}");
+    enemy
+}
 
 pub struct ApiState {
-    pub enemies: Vec<Enemy>, // keep them in memory to avoid constant reads.
+    pub enemies: Box<[Enemy]>,
     pub daily_enemy: Enemy
 }
 
 pub fn make_router() -> Router {
     Router::new()
         .route("/daily/diff/{id}", get(daily_diff))
+        .layer(CorsLayer::new().allow_methods([Method::GET]).allow_origin(Any))
         .with_state(Arc::new(ApiState { 
-            daily_enemy: get_random_enemy(), enemies: get_all_enemies() 
+            daily_enemy: get_random_enemy(), enemies: list_enemies() 
         }))
 }
 
